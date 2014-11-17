017367cbabcee02f9f684271c333dc4341df866e
Kishor Bikram Oli <olikishor@outlook.com>
2014-11-18 00:23:19 +0545

    resolved comment tags description issues

--- FacilityDocu.UI.Store/CameraOpen.xaml.cs
+++ FacilityDocu.UI.Store/CameraOpen.xaml.cs
@@ -155,17 +155,26 @@ namespace Tablet_App
         private void Edit_btn_Tapped(object sender, TappedRoutedEventArgs e)
         {
             gdvImageDetailEdit.Visibility = Visibility.Visible;
+            txtComment.Text = "";
+            txtImageDescription.Text = "";
+            txtTags.Text = "";
         }
 
         private void discard_New_btn_Tapped(object sender, TappedRoutedEventArgs e)
         {
             gdvPreview.Visibility = Visibility.Collapsed;
+            txtComment.Text = "";
+            txtImageDescription.Text = "";
+            txtTags.Text = "";
         }
 
         private void Save_New_btn_Tapped(object sender, TappedRoutedEventArgs e)
         {
             Data.CURRENT_ACTION.Images.Add(currentImage);
             gdvPreview.Visibility = Visibility.Collapsed;
+            txtComment.Text = "";
+            txtImageDescription.Text = "";
+            txtTags.Text = "";
         }
 
         private void Save_Next_btn_Tapped(object sender, TappedRoutedEventArgs e)
@@ -196,6 +205,7 @@ namespace Tablet_App
                             Data.CURRENT_MODULE = Data.CURRENT_RIG.Modules[0];
                             Data.CURRENT_STEP = Data.CURRENT_MODULE.Steps[0];
                             Data.CURRENT_ACTION = Data.CURRENT_STEP.Actions[0];
+                            gdvPreview.Visibility = Visibility.Collapsed;
                         }
                     }
                     else
@@ -203,10 +213,12 @@ namespace Tablet_App
                         Data.CURRENT_MODULE = Data.CURRENT_RIG.Modules[currentModuleIdex + 1];
                         Data.CURRENT_STEP = Data.CURRENT_MODULE.Steps[0];
                         Data.CURRENT_ACTION = Data.CURRENT_STEP.Actions[0];
+                        gdvPreview.Visibility = Visibility.Collapsed;
                     }
                 }
                 else
                 {
+                    gdvPreview.Visibility = Visibility.Collapsed;
                     Data.CURRENT_STEP = Data.CURRENT_MODULE.Steps[currentStepIdex + 1];
                     Data.CURRENT_ACTION = Data.CURRENT_STEP.Actions[0];
                 }
@@ -214,10 +226,13 @@ namespace Tablet_App
             else
             {
                 Data.CURRENT_ACTION = Data.CURRENT_STEP.Actions[currentActionIdex + 1];
+                gdvPreview.Visibility = Visibility.Collapsed;
             }
 
-            gdvPreview.Visibility = Visibility.Collapsed;
             ChangeScreenControls();
+            txtComment.Text = "";
+            txtImageDescription.Text = "";
+            txtTags.Text = "";
         }
 
         private void Save_Close_btn_Tapped(object sender, TappedRoutedEventArgs e)
@@ -227,6 +242,10 @@ namespace Tablet_App
             WriteImages();
             media.Dispose();
             this.Frame.Navigate(typeof(MainPage));
+            txtComment.Text = "";
+            txtImageDescription.Text = "";
+            txtTags.Text = "";
+            ScreenMessage.Show("Successfully saved!");
         }
 
         private void WriteImages()
